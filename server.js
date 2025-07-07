7ae8384943a2ab08e0989a7a665fb45e20288adf
rupendravish14 <rupendravishwakarma921@gmail.com>
2025-07-07 11:02:20 +0530

    cors varibbale add

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ app.use(helmet());
 const allowedOrigins = [
   "http://localhost:3000", // Create React App default
   "http://localhost:5173", // Vite default
-  "http://127.0.0.1:3000",
-  "http://127.0.0.1:5173",
+  "https://groenv8.com",
+  "https://newgeofrontend.vercel.app",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
